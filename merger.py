dee44a84fbbccd47386a30146c720035ec80660a
zachvalenta <zjv5002@gmail.com>
2018-10-17 23:08:49 -0400

    add requirements.txt

--- merger.py
+++ merger.py
@@ -1,5 +1,5 @@
 import json
-
+import requests
 
 def open_file():
 	with open('ms_data_sample.json') as f:
@@ -20,9 +20,13 @@ def get_first_last_name(l):
 		first = names[0]
 		last = names[len(names) - 1]
 		nl.append(set([first, last]))
-	print(type(nl))
 	return nl
 
 
+def lookup_npi(l):
+	return 'foo'
+
+
 provider_names = get_full_name(open_file())
-get_first_last_name(provider_names)
+provider_names_discrete = get_first_last_name(provider_names)
+npi = lookup_npi(provider_names_discrete)
